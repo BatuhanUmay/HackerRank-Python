d110bd889d4269cb7d50b758071038bc3b427e56
batuhanumay <47949233+BatuhanUmay@users.noreply.github.com>
2022-02-05 21:32:41 +0300

    15.gün

---
+++ Easy/Write a function.py
@@ -0,0 +1,20 @@
+# def is_leap(year):
+#     leap = False
+#
+#     if year % 4 == 0:
+#         leap = True
+#         if year % 100 == 0:
+#             leap = False
+#             if year % 400 == 0:
+#                 leap = True
+#     return leap
+#
+# year = int(input())
+# print(is_leap(year))
+
+#################################################################################
+def is_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+
+year = int(input())
+print(is_leap(year))
