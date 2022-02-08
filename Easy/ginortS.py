e420ce7eb9c61947f7917774b3fca2b9c9b4687a
batuhanumay <47949233+BatuhanUmay@users.noreply.github.com>
2022-02-08 22:33:37 +0300

    16.gün

--- Easy/ginortS.py
+++ Easy/ginortS.py
@@ -20,5 +20,18 @@ def control(str):
 
 print(control(input()))
 ##################################################################################
-
 # print(*(sorted(input(), key=lambda x: (x.isdigit(), x.isdigit() and int(x)%2==0, x.isupper(), x.islower(), x))), sep='')
+##################################################################################
+
+# def getKey(x):
+#     if x.islower():
+#         return(1,x)
+#     elif x.isupper():
+#         return(2,x)
+#     elif x.isdigit() :
+#         if int(x)%2==1:
+#             return(3,x)
+#         else :
+#             return(4,x)
+#
+# print(*sorted(input(),key=getKey),sep='')
